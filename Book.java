daf0fea3bd4298e16deca263035a7f8f272ba29f
Emil Valde <zordaso@gmail.com>
2018-02-13 12:09:44 +0100

    Initial sharing of project

---
+++ Book.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class BookInventory here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Book
+{
+    private String title;
+    private String author;
+    private String publisher;
+    private String genre;
+    private int releaseYear;
+    /**
+     * Constructor for objects of class BookInventory
+     */
+    public Book()
+    {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+    
+    public String getTitle ()
+    {
+        return this.title;
+    }
+}

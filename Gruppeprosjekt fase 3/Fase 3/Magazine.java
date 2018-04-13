585403f0364609cc337327eaffea4ac86e5b3b7c
emival08 <36442123+emival08@users.noreply.github.com>
2018-04-13 17:04:33 +0200

    Add files via upload

---
+++ Gruppeprosjekt fase 3/Fase 3/Magazine.java
@@ -0,0 +1,33 @@
+/**
+ * Classifies a book.
+ *
+ * @author (Pernille, Emil og Ørjan)
+ * @version (Version nummer 0.1)
+ *
+ **/
+ 
+ public class Magazine extends Literature
+ {
+    private String releaseDate;
+     
+    public Magazine(String title, String author, String publisher,String releaseDate, String edition)
+    {
+        super(title,author,publisher,edition);
+        this.releaseDate = releaseDate;
+    }
+    
+       public String getReleaseDate() 
+   {
+    return releaseDate;
+   }
+    
+     public String getMagazineInformation()
+    {
+      return "\nTitle: " + getTitle() + 
+      "\nAuthor: " + getAuthor() +
+      "\nPublisher: " + getPublisher() +
+      "\nRelease Year: " + getReleaseDate() +
+      "\nEdition:" + getEdition();
+    }
+    
+ }
---
+++ Gruppeprosjekt fase 3/Fase 3/Registry.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+/**
+ * 
+ * Controll-class that maintains the bookregistry.
+ * 
+ *
+ * @author (Pernille, Emil og Ørjan)
+ * @version (Version nummer 0.1)
+ */
+
+public class Registry
+{
+    private ArrayList<Book> bookLibrary;
+    private ArrayList<Magazine> magazineLibrary;
+    
+    /**
+     * Constructor for objects of the class "Registry".
+     */
+    public Registry()
+    {
+        this.bookLibrary = new ArrayList<Book>();
+        this.magazineLibrary = new ArrayList<Magazine>();
+    }
+    /**
+     * Adds a new magazine to the registry
+     */
+    public void addNewMagazine(Magazine magazine)
+    {
+        this.magazineLibrary.add(magazine);
+    }
+    /**
+     * List all the magazines in the library
+     */
+    public void listAllMagazines()
+    {
+        for(Magazine m:this.magazineLibrary)
+        {
+            System.out.println(m.getMagazineInformation());
+        }
+    }
+    /**
+     * Adds a new book to the registry.
+     */
+    public void addNewBook(Book book)
+    {
+        this.bookLibrary.add(book);
+    }
+    /**
+     * Lists all books in the registry by the order of the parameters.
+     */
+    public void listAllBooks()
+    {
+        
+        for(Book b:this.bookLibrary)
+        {
+            System.out.println(b.getBookInformation());
+        }
+        
+    }
+    public Magazine findMagazineByTitle(String title)
+    {
+        Magazine foundMagazine = null;
+        
+        Iterator<Magazine> it = this.magazineLibrary.iterator();
+        while ((null == foundMagazine) && (it.hasNext()))
+        {
+            Magazine m = it.next();
+            if(m.getTitle().equals(title))
+            {
+                foundMagazine = m;
+            }
+         
+        }
+        return foundMagazine;
+    }
+    
+    /**
+     * Find and returns the book matching the parameter of the method.
+     * If there are no titles matching the parameter, then it returns "null".
+     */
+    public Book findBookByTitle(String title)
+    {
+        Book foundBook = null;
+        
+        Iterator<Book> it = this.bookLibrary.iterator();
+        while ((null == foundBook) && (it.hasNext()))
+        {
+            Book b = it.next();
+            if (b.getTitle().equals(title))
+            {
+                foundBook = b;
+            }
+        }
+        return foundBook;
+    }
+    
+    /**
+     * Removes the book with a title matching the title given by the parameter from the book library.
+     * The title being removed will be returned by the method.
+     * 
+     * @param //write the title of the book to remove.
+     */
+    public Book removeBookByTitle(String title)
+    {
+        Book bookDeleted = null;
+        
+        Iterator<Book> it = this.bookLibrary.iterator();
+        while ((null == bookDeleted) && (it.hasNext()))
+        {
+            Book b = it.next();
+            if (b.getTitle().equals(title))
+            {
+                bookDeleted = b;
+                it.remove();
+            }
+        }
+        return bookDeleted;
+    }
+    public boolean bookLibraryIsEmpty()
+    {
+        return this.bookLibrary.isEmpty();
+    }
+    
+    public boolean magazineLibraryIsEmpty()
+    {
+        return this.magazineLibrary.isEmpty();
+    }
+}

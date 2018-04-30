929ee869151c00cf6652de90659372c07cff8aa0
persky1 <36442101+persky1@users.noreply.github.com>
2018-04-30 10:33:22 +0200

    javafx 2

---
+++ testtest/src/sample/Magazine.java
@@ -0,0 +1,36 @@
+package sample;
+
+/**
+ * Classifies a magazine.
+ *
+ * @author (Pernille, Emil og Ørjan)
+ * @version (Version nummer 0.1)
+ *
+ **/
+ 
+ 
+ public class Magazine extends Literature
+ {
+    private String releaseDate;
+     
+    public Magazine(String title, String author, String publisher,String releaseDate, String edition)
+    {
+        super(title,author,publisher,edition);
+        this.releaseDate = releaseDate;
+    }
+    
+       public String getReleaseDate() 
+   {
+    return releaseDate;
+   }
+    
+     public String getLiteratureInformation()
+    {
+      return "\nTitle: " + getTitle() + 
+      "\nAuthor: " + getAuthor() +
+      "\nPublisher: " + getPublisher() +
+      "\nRelease date: " + getReleaseDate() +
+      "\nEdition:" + getEdition();
+    }
+    
+ }

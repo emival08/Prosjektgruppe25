d0bcd568776cb07ad272a7fa8648b7baa9e66303
persky1 <36442101+persky1@users.noreply.github.com>
2018-05-03 04:57:54 +0200

    Add files via upload

---
+++ litkioskgui2/litkioskgui2/src/literature/Literature.java
@@ -0,0 +1,40 @@
+package src.literature;
+
+
+/**
+ * Classifies the literature
+ *
+ * @author (Pernille, Emil og Ørjan)
+ * @version (Version nummer 0.1)
+ *
+ **/
+
+
+public abstract class Literature {
+    private String title;
+    private String publisher;
+    private String edition;
+
+    public Literature(String title, String author, String publisher, String edition) {
+        this.title = title;
+        this.publisher = publisher;
+        this.edition = edition;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public abstract String getLiteratureInformation();
+
+}
+ 
---
+++ litkioskgui2/litkioskgui2/src/ui/AlertBox.java
@@ -0,0 +1,51 @@
+package src.ui;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.StageStyle;
+import java.util.Optional;
+
+public class AlertBox {
+
+    public static void displayInfo(String title, String header, String message) {
+        //window.initModality(Modality.APPLICATION_MODAL);
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+
+    public static void displayConf (String title, String header, String message) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            alert.close();
+        } else {
+            alert.close();
+        }
+    }
+
+    public static boolean displayConfC() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you want to close this window?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            return true;
+        } else {
+            alert.close();
+        }
+        return false;
+    }
+
+
+}
